ab33e6c7f9fb1f5f8eddbaa9ffffc8f341ef6862
Bruno Lui <bruno.lui@bluesoft.com.br>
2014-03-20 10:23:04 -0300

    Alterações para o desafio do Midea

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-var redditList = angular.module('redditList', []);
+var redditList = angular.module('redditList', ['ui.bootstrap']);
 
 redditList.controller('redditController', function($scope, Reddit) { 
-	$scope.title = "Reddit Posts"
+
+	$scope.title = "Reddit Posts";
+	$scope.favorites = [];
 	
 	Reddit.get(function(posts){
 	    $scope.posts = posts.data.children;
 	});
-	
 
+	$scope.favoritar = function(post) {
+		$scope.favorites.push(post);
+	};
+	
+	$scope.desfavoritar = function(post) {
+		$scope.favorites.pop(post);
+	};
 });
 
 redditList.factory("Reddit", function($http){
   return {
     get: function(callback) {
       return $http.get("http://www.reddit.com/.json").success(callback);
+    }
+  }
+});
+
+redditList.directive("lookPost", function($modal) {
+  return {
+    restrict: "E",
+    scope: {
+      postagem: "="
+    },
+    template: '<img src="{{postagem.data.thumbnail}}" ng-click="abrePost()"></img>',
+    link: function(scope, element, attr) {
+
+      scope.abrePost = function() {
+          var modalInstance = $modal.open({
+            templateUrl: 'modal.html',
+            resolve: {
+              post: function(){ return scope.postagem; }
+            }
+          });
+
+      };
     }
   }
 });
